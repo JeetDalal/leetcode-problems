1b7bfd9c1e1c3a55777b5b4da1cada820da80c3c
JeetDalal <93593029+JeetDalal@users.noreply.github.com>
2024-01-13 22:06:49 +0400

    Time: 0 ms (100.00%), Space: 14.3 MB (6.44%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -29,11 +29,6 @@ public:
         if(v1.size() != v2.size()){
             return false;
         }
-        for(int i=0;i<v1.size();i++){
-            if(v1[i] != v2[i]){
-                return false;
-            }
-        }
-        return true;
+        return v1==v2;
     }
 };
