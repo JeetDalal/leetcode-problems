acb90e9235bca6cdfb3b18efca45ff91850c7090
JeetDalal <93593029+JeetDalal@users.noreply.github.com>
2024-02-17 00:36:14 +0530

    Time: 153 ms (51.22%), Space: 66.1 MB (51.33%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    static bool fcompare(pair<int, pair<int, int> > p,
-              pair<int, pair<int, int> > p1)
-{
-    if (p.second.second != p1.second.second)
-        return (p.second.second > p1.second.second);
-    else
-        return (p.second.first < p1.second.first);
-}
-    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        unordered_map<int, pair<int, int> > hash; 
-       
-    for (int i = 0; i < arr.size(); i++) {
-        if (hash.find(arr[i]) != hash.end())
-            hash[arr[i]].second++;
-        else{
-            hash[arr[i]] = make_pair(i, 1);
-          
-        }
-            
-    } 
-    
-    vector<pair<int, pair<int, int> > > b;
-    for (auto &it:hash)
-        b.push_back(make_pair(it.first, it.second));
- 
-    sort(b.begin(), b.end(), fcompare);
-    while(k>0){
-        int val = b[b.size()-1].first;
-        cout<<val<<" ";  
-        if(hash.find(val) != hash.end()){
-            if(hash[val].second == 1){
-                b.pop_back();
-                hash.erase(val);
-            }else{
-                hash[val].second--;
-            }
-        }
-        k--;
+    static bool compare(int &a,int &b){
+        return a>b;
     }
-     return b.size();  
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int,int> m;
+        vector<int> f;
+        for(auto &val:arr){
+            m[val]++;
+        }
+        for(auto &val:m){
+            f.push_back(val.second);
+        }
+        // cout<<f.size();
+        sort(f.begin(),f.end(),compare);
+        while(k>0){
+            if(f[f.size()-1]==1){
+                cout<<f[f.size()-1]<<" ";
+                f.pop_back();
+            }else{
+                f[f.size()-1]--;
+            }
+            k--;
+        }
+        return f.size();
+        
     }
 };
