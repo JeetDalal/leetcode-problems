cba9c9ed0015d61ff32dd9833a88b8b5097b1cac
JeetDalal <93593029+JeetDalal@users.noreply.github.com>
2024-04-21 09:48:03 +0530

    Time: 3 ms (70.48%), Space: 12.1 MB (45.54%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int searchInsert(vector<int>& v, int target) {
-        for(int i=0;i<v.size();i++){
+        int n=v.size();
+        for(int i=0;i<n;i++){
             if(v[i]>=target){
                 return i;
             }
             
         }
-        return v.size();
+        return n;
     }
 };
