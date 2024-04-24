163c7388521bf99de0aab7781a9b812e0f7020fd
JeetDalal <93593029+JeetDalal@users.noreply.github.com>
2024-04-24 11:09:16 +0530

    Time: 3 ms (22.77%), Space: 7.4 MB (13.08%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int solve(int n, vector<int>& v){
-        if(n == 0){
-            return 0;
-        }
-        if(n < 3){
-            return 1;
-        }
+        
         if(v[n] != -1){
             return v[n];
         }
@@ -16,11 +11,14 @@ public:
     }
     
     int tribonacci(int n) {
-        if(n == 0){
+       if(n == 0){
             return 0;
         }
+        if(n < 3){
+            return 1;
+        }
         
-        vector<int> v(max(n + 1, 3), -1);  // Ensure vector size is at least max(n+1, 3)
+        vector<int> v(n+1, -1);  
         v[0] = 0;
         v[1] = v[2] = 1;
         int result = solve(n, v);
