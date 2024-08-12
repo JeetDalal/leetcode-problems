87d2d888becff04b291a15aab48d1d382425db39
JeetDalal <93593029+JeetDalal@users.noreply.github.com>
2024-08-12 18:09:41 +0530

    Time: 31 ms (6.33%), Space: 17 MB (20.09%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool bfs(int node,vector<int> &visited,vector<vector<int>> &graph){
+        int initialColor=0;
+        visited[node] = 0;
+        queue<vector<int>> q;
+        q.push({node,initialColor});
+        
+        while(!q.empty()){
+            int currentNode = q.front()[0];
+            int currentColor = q.front()[1];
+            // int currentKaParent = q.front()[2];
+            q.pop();
+            for(auto it: graph[currentNode]){
+                if(visited[it]== -1){
+                    cout<<it<<" "<<currentColor<<endl;
+                    visited[it] = !currentColor;
+                    q.push({it,visited[it]});
+                }else{
+                    if(currentColor == visited[it]) return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        int m = graph[0].size();
+        vector<int> visited(n+1,-1);
+        
+        for(int i=0;i<n;i++){
+            if(visited[i]== -1){
+                int ans = bfs(i,visited,graph);
+                if(!ans) return false;
+            }
+        }
+        return true;
+        
+    }
+};
